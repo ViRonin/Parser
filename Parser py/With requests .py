0c480bfc2da2d934c84863d16225e1a71660fb42
ViRonin <77367183+ViRonin@users.noreply.github.com>
2021-12-16 11:53:53 +0300

    Update With requests .py

--- Parser py/With requests .py
+++ Parser py/With requests .py
@@ -6,12 +6,12 @@ def get_html():
     r = requests.get('https://prom.ua/Dom-i-sad') #Менять ссылку тут
     return r.text
 
-
 soup = BeautifulSoup(get_html() , 'html.parser')
 content = soup.find_all('div', class_='x-catalog__content-line') #'div' 
 last_try = content.find_all('div', class_='x-gallery-tile__content')
 art_list = [] #Лист со статьями
 i=0
+
 for article in last_try:
     name = article.find('a', class_='x-gallery-tile__name').text #тоже работает 
     name = article.find('span', {'itemprop': 'name'}).text
@@ -19,5 +19,6 @@ for article in last_try:
     price = article.find('div', class_='x-gallery-tile__price')['data-qaprice']
     link = article.find('a', class_='x-gallery-tile__name')['href']
     art_list.append([link,price,name])
+
 print(art_list)
 print("================ ВЖУХ  a(＾O ωO＾)a (((((((((●～* ГОТОВО ================")
